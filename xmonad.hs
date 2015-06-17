8cd479820bf6d39ff58c321ce47b6f3ff94bcf88
Keegan Myers <keeganmyers608@gmail.com>
2015-06-17 17:49:52 -0500

    use gnome configuration

--- xmonad.hs
+++ xmonad.hs
@@ -1,19 +1,3 @@
-{-
-  This is my xmonad configuration file.
-  There are many like it, but this one is mine.
-
-  If you want to customize this file, the easiest workflow goes
-  something like this:
-    1. Make a small change.
-    2. Hit "super-q", which recompiles and restarts xmonad
-    3. If there is an error, undo your change and hit "super-q" again to
-       get to a stable place again.
-    4. Repeat
-
-  Author:     David Brewer
-  Repository: https://github.com/davidbrewer/xmonad-ubuntu-conf
--}
-
 import XMonad
 import XMonad.Hooks.SetWMName
 import XMonad.Layout.Grid
@@ -33,12 +17,8 @@ import XMonad.Hooks.UrgencyHook
 import XMonad.Hooks.ICCCMFocus
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
+import XMonad.Config.Gnome
 import Data.Ratio ((%))
-
-{-
-  Xmonad configuration variables. These settings control some of the
-  simpler parts of xmonad's behavior and are straightforward to tweak.
--}
 
 myModMask            = mod4Mask       -- changes the mod key to "super"
 myFocusedBorderColor = "#ff0000"      -- color of focused border
@@ -49,43 +29,6 @@ myIMRosterTitle      = "Buddy List"   -- title of roster on IM workspace
                                       -- use "Buddy List" for Pidgin, but
                                       -- "Contact List" for Empathy
 
-
-{-
-  Xmobar configuration variables. These settings control the appearance
-  of text which xmonad is sending to xmobar via the DynamicLog hook.
--}
-
-myTitleColor     = "#eeeeee"  -- color of window title
-myTitleLength    = 80         -- truncate window title to this length
-myCurrentWSColor = "#e6744c"  -- color of active workspace
-myVisibleWSColor = "#c185a7"  -- color of inactive workspace
-myUrgentWSColor  = "#cc0000"  -- color of workspace with 'urgent' window
-myCurrentWSLeft  = "["        -- wrap active workspace with these
-myCurrentWSRight = "]"
-myVisibleWSLeft  = "("        -- wrap inactive workspace with these
-myVisibleWSRight = ")"
-myUrgentWSLeft  = "{"         -- wrap urgent workspace with these
-myUrgentWSRight = "}"
-
-
-{-
-  Workspace configuration. Here you can change the names of your
-  workspaces. Note that they are organized in a grid corresponding
-  to the layout of the number pad.
-
-  I would recommend sticking with relatively brief workspace names
-  because they are displayed in the xmobar status bar, where space
-  can get tight. Also, the workspace labels are referred to elsewhere
-  in the configuration file, so when you change a label you will have
-  to find places which refer to it and make a change there as well.
-
-  This central organizational concept of this configuration is that
-  the workspaces correspond to keys on the number pad, and that they
-  are organized in a grid which also matches the layout of the number pad.
-  So, I don't recommend changing the number of workspaces unless you are
-  prepared to delve into the workspace navigation keybindings section
-  as well.
--}
 
 myWorkspaces =
   [
@@ -149,56 +92,16 @@ defaultLayouts = smartBorders(avoidStruts(
   ||| Grid))
 
 
--- Here we define some layouts which will be assigned to specific
--- workspaces based on the functionality of that workspace.
 
--- The chat layout uses the "IM" layout. We have a roster which takes
--- up 1/8 of the screen vertically, and the remaining space contains
--- chat windows which are tiled using the grid layout. The roster is
--- identified using the myIMRosterTitle variable, and by default is
--- configured for Pidgin, so if you're using something else you
--- will want to modify that variable.
 chatLayout = avoidStruts(withIM (1%7) (Title myIMRosterTitle) Grid)
 
--- The GIMP layout uses the ThreeColMid layout. The traditional GIMP
--- floating panels approach is a bit of a challenge to handle with xmonad;
--- I find the best solution is to make the image you are working on the
--- master area, and then use this ThreeColMid layout to make the panels
--- tile to the left and right of the image. If you use GIMP 2.8, you
--- can use single-window mode and avoid this issue.
 gimpLayout = smartBorders(avoidStruts(ThreeColMid 1 (3/100) (3/4)))
 
--- Here we combine our default layouts with our specific, workspace-locked
--- layouts.
 myLayouts =
   onWorkspace "7:Chat" chatLayout
   $ onWorkspace "9:Pix" gimpLayout
   $ defaultLayouts
 
-
-{-
-  Custom keybindings. In this section we define a list of relatively
-  straightforward keybindings. This would be the clearest place to
-  add your own keybindings, or change the keys we have defined
-  for certain functions.
-
-  It can be difficult to find a good list of keycodes for use
-  in xmonad. I have found this page useful -- just look
-  for entries beginning with "xK":
-
-  http://xmonad.org/xmonad-docs/xmonad/doc-index-X.html
-
-  Note that in the example below, the last three entries refer
-  to nonstandard keys which do not have names assigned by
-  xmonad. That's because they are the volume and mute keys
-  on my laptop, a Lenovo W520.
-
-  If you have special keys on your keyboard which you
-  want to bind to specific actions, you can use the "xev"
-  command-line tool to determine the code for a specific key.
-  Launch the command, then type the key in question and watch
-  the output.
--}
 
 myKeyBindings =
   [
@@ -214,49 +117,6 @@ myKeyBindings =
     , ((0, 0x1008FF13), spawn "amixer -q set Master 10%+")
   ]
 
-
-{-
-  Management hooks. You can use management hooks to enforce certain
-  behaviors when specific programs or windows are launched. This is
-  useful if you want certain windows to not be managed by xmonad,
-  or sent to a specific workspace, or otherwise handled in a special
-  way.
-
-  Each entry within the list of hooks defines a way to identify a
-  window (before the arrow), and then how that window should be treated
-  (after the arrow).
-
-  To figure out to identify your window, you will need to use a
-  command-line tool called "xprop". When you run xprop, your cursor
-  will temporarily change to crosshairs; click on the window you
-  want to identify. In the output that is printed in your terminal,
-  look for a couple of things:
-    - WM_CLASS(STRING): values in this list of strings can be compared
-      to "className" to match windows.
-    - WM_NAME(STRING): this value can be compared to "resource" to match
-      windows.
-
-  The className values tend to be generic, and might match any window or
-  dialog owned by a particular program. The resource values tend to be
-  more specific, and will be different for every dialog. Sometimes you
-  might want to compare both className and resource, to make sure you
-  are matching only a particular window which belongs to a specific
-  program.
-
-  Once you've pinpointed the window you want to manipulate, here are
-  a few examples of things you might do with that window:
-    - doIgnore: this tells xmonad to completely ignore the window. It will
-      not be tiled or floated. Useful for things like launchers and
-      trays.
-    - doFloat: this tells xmonad to float the window rather than tiling
-      it. Handy for things that pop up, take some input, and then go away,
-      such as dialogs, calculators, and so on.
-    - doF (W.shift "Workspace"): this tells xmonad that when this program
-      is launched it should be sent to a specific workspace. Useful
-      for keeping specific tasks on specific workspaces. In the example
-      below I have specific workspaces for chat, development, and
-      editing images.
--}
 
 myManagementHooks :: [ManageHook]
 myManagementHooks = [
@@ -274,21 +134,6 @@ myManagementHooks = [
   ]
 
 
-{-
-  Workspace navigation keybindings. This is probably the part of the
-  configuration I have spent the most time messing with, but understand
-  the least. Be very careful if messing with this section.
--}
-
--- We define two lists of keycodes for use in the rest of the
--- keyboard configuration. The first is the list of numpad keys,
--- in the order they occur on the keyboard (left to right and
--- top to bottom). The second is the list of number keys, in an
--- order corresponding to the numpad. We will use these to
--- make workspace navigation commands work the same whether you
--- use the numpad or the top-row number keys. And, we also
--- use them to figure out where to go when the user
--- uses the arrow keys.
 numPadKeys =
   [
     xK_KP_Home, xK_KP_Up, xK_KP_Page_Up
@@ -305,11 +150,6 @@ numKeys =
     , xK_0, xK_minus, xK_equal
   ]
 
--- Here, some magic occurs that I once grokked but has since
--- fallen out of my head. Essentially what is happening is
--- that we are telling xmonad how to navigate workspaces,
--- how to send windows to different workspaces,
--- and what keys to use to change which monitor is focused.
 myKeys = myKeyBindings ++
   [
     ((m .|. myModMask, k), windows $ f i)
@@ -330,39 +170,7 @@ myKeys = myKeyBindings ++
   ]
 
 
-{-
-  Here we actually stitch together all the configuration settings
-  and run xmonad. We also spawn an instance of xmobar and pipe
-  content into it via the logHook.
--}
-
 main = do
-  xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc"
-  xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig {
-    focusedBorderColor = myFocusedBorderColor
-  , normalBorderColor = myNormalBorderColor
-  , terminal = myTerminal
-  , borderWidth = myBorderWidth
-  , layoutHook = myLayouts
-  , workspaces = myWorkspaces
-  , modMask = myModMask
-  , handleEventHook = fullscreenEventHook
-  , startupHook = do
-      setWMName "LG3D"
-      windows $ W.greedyView startupWorkspace
-      spawn "~/.xmonad/startup-hook"
-  , manageHook = manageHook defaultConfig
-      <+> composeAll myManagementHooks
-      <+> manageDocks
-  , logHook = takeTopFocus <+> dynamicLogWithPP xmobarPP {
-      ppOutput = hPutStrLn xmproc
-      , ppTitle = xmobarColor myTitleColor "" . shorten myTitleLength
-      , ppCurrent = xmobarColor myCurrentWSColor ""
-        . wrap myCurrentWSLeft myCurrentWSRight
-      , ppVisible = xmobarColor myVisibleWSColor ""
-        . wrap myVisibleWSLeft myVisibleWSRight
-      , ppUrgent = xmobarColor myUrgentWSColor ""
-        . wrap myUrgentWSLeft myUrgentWSRight
-    }
-  }
+    xmonad $ gnomeConfig
+
     `additionalKeys` myKeys
